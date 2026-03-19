3867f2c83d88c51b9713e6eb34701a5b8e29d381
offleash-dev <152575678+offleash-dev@users.noreply.github.com>
2026-03-18 20:10:22 -0700

    new container class that will be a mix in for screens and panels to consistenly manage widgets/panels

---
+++ src/AFContainer.cpp
@@ -0,0 +1,37 @@
+//// AFContainer.cpp
+//// Part of the AWFUI library
+////
+//// Implementation of widget container interface
+////
+//// Copyright (c) 2026 Matt Foster
+//// Licensed under the MIT License. See LICENSE file for details.
+
+#include "AFContainer.h"
+#include "AFWidget.h"
+#include "AFPanel.h"
+
+
+
+// Helper implementation for widgets
+//
+void AFContainer::markIntersectingDirty(etl::vector<AFWidget*, MAX_WIDGETS_PER_PANEL>& widgets, 
+                                       int16_t rx, int16_t ry, int16_t rw, int16_t rh) {
+    for (auto* widget : widgets) {
+        if (widget && widget->intersects(rx, ry, rw, rh)) {
+            widget->markDirty();
+        }
+    }
+}
+
+
+
+// Helper implementation for panels
+//
+void AFContainer::markIntersectingDirty(etl::vector<AFPanel*, MAX_PANELS_PER_SCREEN>& panels, 
+                                       int16_t rx, int16_t ry, int16_t rw, int16_t rh) {
+    for (auto* panel : panels) {
+        if (panel && panel->intersects(rx, ry, rw, rh)) {
+            panel->markDirty();
+        }
+    }
+}
