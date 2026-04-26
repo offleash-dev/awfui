d6fa46d776c5e403eb4a792ece3a45ef83c11130
offleash-dev <152575678+offleash-dev@users.noreply.github.com>
2026-04-25 22:25:06 -0700

    header include cleanup

--- src/AFBase.cpp
+++ src/AFBase.cpp
@@ -1,5 +1,3 @@
-#include "AFBase.h"
-
 //// AFBase.cpp
 //// Part of the AWFUI library
 ////
@@ -12,11 +10,12 @@
 #include <stdint.h>
 
 #include "AFBase.h"
-#include "AFWidget.h"
-#include "AFPanel.h"
+
 
 
 static char* tempIdBuffer5 = new char[5];
+
+
 
 char* getIdAsCharPtr(uint32_t id) {
       getIdAsChars(id, tempIdBuffer5);
