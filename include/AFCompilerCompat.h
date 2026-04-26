edffdcd83d29225fd60fcb04387925828d1f0927
offleash-dev <152575678+offleash-dev@users.noreply.github.com>
2026-04-25 22:21:08 -0700

    comment fixes

--- include/AFCompilerCompat.h
+++ include/AFCompilerCompat.h
@@ -1,18 +1,19 @@
 #pragma once
 
-//// AFCheckbox.h
+//// AFCompilerCompat.h
 //// Part of the AWFUI library
 ////
-//// Lightweight checkbox widget for embedded UIs.
+//// Compiler compatibility definitions for embedded UIs.
 ////
 //// Copyright (c) 2026 Matt Foster
 //// Licensed under the MIT License. See LICENSE file for details.
-#ifndef AF_COMPILER_COMPAT_H
-#define AF_COMPILER_COMPAT_H
+
 
 // =============================================================================
-// Compiler Compatibility - Force-included before all source files
+// Compiler Compatibility - to be included before all source files to manage 
+// inconsistent definitions between platforms and compilers.
 // =============================================================================
+
 
 // MSVC doesn't have GCC's __builtin_bswap* - provide equivalents
 #ifdef _MSC_VER
@@ -21,5 +22,3 @@
 #define __builtin_bswap32(x) _byteswap_ulong(x)
 #define __builtin_bswap64(x) _byteswap_uint64(x)
 #endif
-
-#endif // AF_COMPILER_COMPAT_H
